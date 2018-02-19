676d79791b65ba282da93830df18a4fb604ade2b
mzgajner <mato@zgajner.com>
2018-02-19 20:21:43 +0100

    Specify more specific type

--- src/sprites/Cage.ts
+++ src/sprites/Cage.ts
@@ -16,6 +16,5 @@ export default class extends Phaser.Group {
     this.game.physics.enable(this, Phaser.Physics.ARCADE);
     this.setAll('body.immovable', true);
     this.setAll('body.allowGravity', false);
-
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const centerGameObjects = (objects: Array<object>) => {
+export const centerGameObjects = (objects: Array<Phaser.Sprite>) => {
   objects.forEach((object) => {
     object.anchor.setTo(0.5);
   });
