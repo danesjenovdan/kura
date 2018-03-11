89468228764e6abcf16fd13ad250196cb900afea
mzgajner <mato@zgajner.com>
2018-03-11 21:24:39 +0100

    Remove unneeded code

--- src/states/Splash.ts
+++ src/states/Splash.ts
@@ -1,15 +1,8 @@
-import { centerGameObjects } from '../utils';
-
 export default class extends Phaser.State {
   loaderBg: Phaser.Sprite
   loaderBar: Phaser.Sprite
 
   preload() {
-    // this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg');
-    // this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar');
-    // centerGameObjects([this.loaderBg, this.loaderBar]);
-
-    // this.load.setPreloadSprite(this.loaderBar);
     this.load.spritesheet('chicken', 'assets/images/chicken.png', 22, 29);
     this.load.image('cage-vertical', 'assets/images/cage-vertical.png');
     this.load.image('cage-horizontal', 'assets/images/cage-horizontal.png');
