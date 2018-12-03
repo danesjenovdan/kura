4608b0aee2720abdbb4b887e611fc74a4bd0d11f
majacimerman <maja@djnd.si>
2018-12-03 14:50:25 +0100

    četrti update

--- src/states/Intro.ts
+++ src/states/Intro.ts
@@ -8,7 +8,7 @@ export default class extends TextScreen {
     this.game.stage.backgroundColor = '#222';
 
     this.nextScreen = 'Intro2';
-    this.textObject.text =  'November 2018 marked 3 years since Slovenia started building razor-wire fence on its southern border. So-called technical barriers did no reduce number of entries, they just made entry more dangerous.\n\Press SPACE or ENTER to continue.';
+    this.textObject.text =  'November 2018 marked 3 years since Slovenia started building razor-wire fence on its southern border. So-called technical barriers did no reduce number of entries, they wire just made entering Slovenia more dangerous.\n\Press SPACE or ENTER to continue.';
     this.textObject.position.y = 78;
 
     const zica = this.game.add.image(112, 38, 'zica');
